e7c69ab0ab6dae010459577261dc251725b20ea3
LinusRingstad <158219472+LinusRingstad@users.noreply.github.com>
2024-02-24 19:06:37 -0600

    Add files via upload

---
+++ code/motortest.py
@@ -0,0 +1,27 @@
+import numpy as np
+from src import motor as motor_module
+import time
+
+if __name__ == '__main__':
+
+    motor1 = motor_module.Motor({
+        "pins": {
+            "speed": 13,
+            "control1": 5,
+            "control2": 6
+        }
+    })
+
+    motor2 = motor_module.Motor({
+        "pins": {
+            "speed": 12,
+            "control1": 7,
+            "control2": 8
+        }
+    })
+    
+dt = 0.25
+speed = 5
+motor1.forward(speed)
+motor2.forward(speed)
+time.sleep(dt)
