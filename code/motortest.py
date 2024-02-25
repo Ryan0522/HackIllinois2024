b8d24cf6bed1976e54a3ae94168c504ad3667a1e
Ryan0522 <123705502+Ryan0522@users.noreply.github.com>
2024-02-25 00:51:30 -0600

    Update motortest.py

--- code/motortest.py
+++ code/motortest.py
@@ -41,7 +41,7 @@ def left_turn():
 def right_full_turn():
     motor1.forward(1)
     motor2.backward(1)
-    time.sleep(0.68) #coefficient on wood
+    time.sleep(0.65) #coefficient on wood
 
 def right_big_full_turn():
     motor1.forward(1)
