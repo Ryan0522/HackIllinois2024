30f3eb9a5828ffa0cddf09bd2563cf33affdd618
Ryan0522 <123705502+Ryan0522@users.noreply.github.com>
2024-02-25 00:57:04 -0600

    Update programme.py

--- code/programme.py
+++ code/programme.py
@@ -248,4 +248,4 @@ if __name__ == '__main__':
         angles.append(calculate_angle(vector1, vector2))
         vectors.append(vector2)
     
-    drive(use_equal, angles, 300)
+    drive(use_equal, angles, 500)
