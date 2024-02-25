70b33479deb2b38358f533bf96e06f501d8a7ea4
Ryan0522 <123705502+Ryan0522@users.noreply.github.com>
2024-02-25 01:05:06 -0600

    Update motortest.py

--- code/motortest.py
+++ code/motortest.py
@@ -29,34 +29,34 @@ if __name__ == '__main__':
     })
 
 def right_turn():
-    motor1.forward(1)
+    motor1.forward(0.925)
     motor2.backward(1)
-    time.sleep(0.355)
+    time.sleep(0.325) # was 0.355
 
 def left_turn():
     motor2.forward(1)
-    motor1.backward(1)
-    time.sleep(0.355) #original on paper
+    motor1.backward(0.925)
+    time.sleep(0.325) #original on paper
 
 def right_full_turn():
-    motor1.forward(1)
+    motor1.forward(0.925)
     motor2.backward(1)
     time.sleep(0.65) #coefficient on wood
 
 def right_big_full_turn():
-    motor1.forward(1)
+    motor1.forward(0.925)
     motor2.forward(0)
-    time.sleep(1.25)
+    time.sleep(1.3)
 
 def left_big_full_turn():
     motor2.forward(1)
     motor1.forward(0)
-    time.sleep(1.25)
+    time.sleep(1.3)
 
 def right_full_circle():
-    motor1.forward(1)
+    motor1.forward(0.925)
     motor2.backward(1)
-    time.sleep(1.42)
+    time.sleep(1.5)
 
 def stop():
     motor1.forward(0)
@@ -80,50 +80,50 @@ def zoom(t):
     motor2.forward(1)
     time.sleep(t)
 
-# zoom(0.9)
+zoom(0.9)
+stop()
+right_turn()
+zoom(0.15)
+left_big_full_turn()
+zoom(0.3)
+left_big_full_turn()
+zoom(0.3)
+left_big_full_turn()
+zoom(0.15)
+right_turn()
+zoom(0.3)
+
+motor1.forward(1)
+motor2.forward(0.3)
+time.sleep(1.45)
+
+zoom(0.65)
+left_big_full_turn()
+zoom(0.35)
+left_big_full_turn()
+zoom(0.75)
+
+right_turn()
+zoom(0.4)
+right_turn()
+zoom(0.4)
+left_turn()
+zoom(0.4)
+left_turn()
+zoom(0.4)
+right_turn()
+zoom(0.4)
 # stop()
-# right_turn()
-# zoom(0.15)
-# left_big_full_turn()
-# zoom(0.3)
-# left_big_full_turn()
-# zoom(0.3)
-# left_big_full_turn()
-# zoom(0.15)
-# right_turn()
-# zoom(0.3)
+# stop()
+# stop()
 
-# motor1.forward(1)
-# motor2.forward(0.3)
-# time.sleep(1.55)
-
-# zoom(0.65)
-# left_big_full_turn()
-# zoom(0.35)
-# left_big_full_turn()
-# zoom(0.75)
-
-# right_turn()
-# zoom(0.4)
-# right_turn()
-# zoom(0.4)
-# left_turn()
-# zoom(0.4)
-# left_turn()
-# zoom(0.4)
-# right_turn()
-# zoom(0.4)
-stop()
-stop()
-stop()
-
-for _ in range(1):
-    zoom(1.4)
-    right_full_turn()
-    pause()
-    zoom(1.4)
-    right_full_turn()
-    pause()
+# for _ in range(1):
+#     zoom(1.4)
+#     right_full_turn()
+#     pause()
+#     zoom(1.4)
+#     right_full_turn()
+#     pause()
 
 motor1.stop()
 motor2.stop()
